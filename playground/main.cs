c6e4fb64caccdbf310259e9e149de67667db600f
Simarpreet Singh <simar05@ca.ibm.com>
2017-03-20 20:58:15 -0400

    sync

--- playground/main.cs
+++ playground/main.cs
@@ -1,13 +1,18 @@
 using System;
 class MainClass
 {
-    public static int i;
+    public static int x;
+    public static int y;
+    public static int z;
     static Boolean b1;
     static void Main(string[] args)
     {
         Boolean b2;
         int i2;
-        i = 2;
+        x = 1;
+        y = 2;
+        z = x + y;
+
         b1 = true;
         Console.WriteLine("Hello World");
     }
