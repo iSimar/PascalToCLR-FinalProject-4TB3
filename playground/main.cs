5f199ab297861dc5dc69861c92e9dd1214f26098
Simarpreet Singh <simar05@ca.ibm.com>
2017-04-08 16:16:40 -0400

    added read, write, writeln support

--- playground/main.cs
+++ playground/main.cs
@@ -2,18 +2,12 @@ using System;
 class MainClass
 {
     public static int x;
-    public static int y;
-    public static int z;
-    static Boolean b1;
     static void Main(string[] args)
     {
-        Boolean b2;
-        int i2;
-        x = 1;
-        y = 2;
-        z = x*3;
-
-        b1 = true;
-        Console.WriteLine("Hello World");
+        x = Convert.ToInt32(Console.ReadLine());
+        x = 3*x;
+        Console.Write(x);
+        Console.WriteLine();
+        Console.WriteLine(x*5);
     }
 }
