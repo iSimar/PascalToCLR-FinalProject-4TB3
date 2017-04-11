d0818bb98d1a276b67815ac8390473499d550b36
Simarpreet Singh <simar05@ca.ibm.com>
2017-04-11 09:53:08 -0400

    sync

--- playground/main.cs
+++ playground/main.cs
@@ -1,34 +1,21 @@
 using System;
 
-class a{
-    public int[] arr = new int[7];
-}
-class r{
-    public int f;
-    public a g = new a();
-    public int h;
-}
 class MainClass
 {
-    public static a v = new a();
-    public static r w = new r();
-    public static int x;
-    
-    class q{
-        public static void main_proc(a c, r d){
-            int y;
-            y = 3;
-            Console.Write(d.h);
-            Console.Write(c.arr[1]);
-            Console.Write(d.g.arr[y]);
-            Console.WriteLine();
-            c.arr[6]=7;
-        }
-    }
+
+    static Boolean t;
+    static Boolean b;
+    static int x;
+    static int y;
     static void Main(string[] args)
     {
-        x = 9;
-        w.g.arr[x/3] = 9;
-        q.main_proc(v, w);
+        t = true;
+        b = (x > y) || t;   
+        if(b){
+            Console.Write(7);
+        }
+        else{
+            Console.Write(9);
+        }
     }
 }
